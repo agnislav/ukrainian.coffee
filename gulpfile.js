bdd150541fcd7ee950adfdb367715149ebb904fa
Agnislav Onufrijchuk <agnislav@gmail.com>
2015-08-08 15:51:41 +0300

    [gulp] tasks reordered in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -84,13 +84,38 @@ gulp.task('build', ['build-index', 'build-styles', 'build-appjs', 'build-scripts
   // todo: put a notification here.
 });
 
-gulp.task('build-index', ['clean-app'], function () {
+/**
+ * TASK: DIST
+ */
+
+gulp.task('dist', ['dist-index.html', 'dist-styles', 'dist-scripts', 'dist-vendor-scripts', 'dist-images', 'dist-partials'], function () {
+  // todo: put a notification here.
+});
+
+/**
+ * TASK: GENERATE INDEX.HTML
+ */
+gulp.task('build-index', ['pre-build-clean'], function () {
   gulp.src(paths.src.index)
     .pipe(jade({ pretty: true}))
     .pipe(gulp.dest(paths.app.root))
 });
 
-gulp.task('build-styles', ['clean-app'], function () {
+gulp.task('dist-index.html', ['pre-dist-clean'], function () {
+  gulp.src(paths.src.index)
+    .pipe(jade({ pretty: true}))
+    .pipe(htmlreplace({
+      styles: 'styles.min.css',
+      scripts: 'app.min.js',
+      vendorscripts: 'vendor.min.js'
+    }))
+    .pipe(gulp.dest(paths.dist.root))
+});
+
+/**
+ * TASK: GENERATE CSS
+ */
+gulp.task('build-styles', ['pre-build-clean'], function () {
   return sass(paths.src.components, { style: 'expanded' , sourcemap: true})
     .on('error', function (err) {
       console.error('Error!', err.message);
@@ -100,68 +125,7 @@ gulp.task('build-styles', ['clean-app'], function () {
     .pipe(gulp.dest(paths.app.components));
 });
 
-gulp.task('build-appjs', ['clean-app'], function () {
-  return gulp.src(paths.src.appjs)
-    .pipe(addStream.obj(generateConfig('development')))
-    .pipe(concat(paths.app.appjs))
-    .pipe(gulp.dest(paths.app.components));
-});
-
-gulp.task('build-scripts', ['clean-app'], function () {
-  return gulp.src(paths.src.js)
-    .pipe(gulp.dest(paths.app.components));
-});
-
-gulp.task('build-vendor-scripts', ['clean-app'], function () {
-  return gulp.src(paths.src.vendorjs)
-    .pipe(gulp.dest(paths.app.vendor));
-});
-
-gulp.task('build-images', ['clean-app'], function () {
-  return gulp.src(paths.src.images)
-    .pipe(gulp.dest(paths.app.images));
-});
-
-gulp.task('build-templates', ['clean-app'], function () {
-  return gulp.src(paths.src.templates)
-    .pipe(gulp.dest(paths.app.root));
-});
-
-gulp.task('build-partials', ['clean-app'], function () {
-  return gulp.src(paths.src.partials)
-    .pipe(gulp.dest(paths.app.root));
-});
-
-gulp.task('clean-app', function (cb) {
-  if (!isWatching) {
-    del(['app/*'], cb)
-  }
-  else cb();
-});
-
-
-/**
- * TASK: DIST
- */
-
-gulp.task('dist', ['dist-index.html', 'dist-styles', 'dist-scripts', 'dist-vendor-scripts', 'dist-images', 'dist-partials'], function () {
-  // todo: put a notification here.
-});
-
-gulp.task('dist-index.html', ['clean-dist'], function () {
-  gulp.src(paths.src.index)
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(htmlreplace({
-      styles: 'styles.min.css',
-      scripts: 'app.min.js',
-      vendorscripts: 'vendor.min.js'
-    }))
-    .pipe(gulp.dest(paths.dist.root))
-});
-
-gulp.task('dist-styles', ['clean-dist'], function () {
+gulp.task('dist-styles', ['pre-dist-clean'], function () {
   //noinspection JSUnresolvedFunction
   return sass(paths.src.components, { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
@@ -170,47 +134,19 @@ gulp.task('dist-styles', ['clean-dist'], function () {
     .pipe(gulp.dest(paths.dist.root));
 });
 
-gulp.task('dist-scripts', ['clean-dist'], function () {
-  return gulp.src(paths.src.appjs)
-    .pipe(addStream.obj(generateConfig('production')))
-    .pipe(addStream.obj(prepareTemplates()))
-    .pipe(addStream.obj(gulp.src(paths.src.js)))
-    .pipe(concat('app.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.dist.root));
-});
-
-gulp.task('dist-vendor-scripts', ['clean-dist'], function () {
-  return gulp.src(paths.src.vendorminjs)
-    .pipe(concat('vendor.min.js'))
-    .pipe(gulp.dest(paths.dist.root));
-});
-
-gulp.task('dist-images', ['clean-dist'], function () {
-  return gulp.src(paths.src.images)
-    .pipe(gulp.dest(paths.dist.images));
-});
-
-// todo [#6]: migrate
-gulp.task('dist-partials', ['clean-dist'], function () {
-  return gulp.src(paths.src.partials)
-    .pipe(gulp.dest(paths.dist.components));
-});
-
-gulp.task('clean-dist', function (cb) {
-  del(['dist/*'], cb)
-});
-
 /**
- HELPERS
+ * TASK: INCLUDE CONFIG TO APP.JS
  */
-function prepareTemplates () {
-  return gulp.src(paths.src.templates)
-    .pipe(angularTemplateCache());
-}
+gulp.task('build-appjs', ['pre-build-clean'], function () {
+  return gulp.src(paths.src.appjs)
+    .pipe(addStream.obj(generateConfig('development')))
+    .pipe(concat(paths.app.appjs))
+    .pipe(gulp.dest(paths.app.components));
+});
+
 
 /**
- *
+ * Generate environment-depending config
  * @param {'production'|'development'} env
  * @returns ReadableStream
  */
@@ -221,3 +157,90 @@ function generateConfig (env) {
       createModule: false
     }));
 }
+
+/**
+ * TASK: PREPARE USER SCRIPTS
+ */
+gulp.task('build-scripts', ['pre-build-clean'], function () {
+  return gulp.src(paths.src.js)
+    .pipe(gulp.dest(paths.app.components));
+});
+
+gulp.task('dist-scripts', ['pre-dist-clean'], function () {
+  return gulp.src(paths.src.appjs)
+    .pipe(addStream.obj(generateConfig('production')))
+    .pipe(addStream.obj(prepareTemplates()))
+    .pipe(addStream.obj(gulp.src(paths.src.js)))
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist.root));
+});
+
+/**
+ * TASK: PREPARE VENDOR SCRIPTS
+ */
+gulp.task('build-vendor-scripts', ['pre-build-clean'], function () {
+  return gulp.src(paths.src.vendorjs)
+    .pipe(gulp.dest(paths.app.vendor));
+});
+
+gulp.task('dist-vendor-scripts', ['pre-dist-clean'], function () {
+  return gulp.src(paths.src.vendorminjs)
+    .pipe(concat('vendor.min.js'))
+    .pipe(gulp.dest(paths.dist.root));
+});
+
+/**
+ * TASK: COPY IMAGES
+ */
+gulp.task('build-images', ['pre-build-clean'], function () {
+  return gulp.src(paths.src.images)
+    .pipe(gulp.dest(paths.app.images));
+});
+
+gulp.task('dist-images', ['pre-dist-clean'], function () {
+  return gulp.src(paths.src.images)
+    .pipe(gulp.dest(paths.dist.images));
+});
+
+/**
+ * TASK: PREPARE TEMPLATES
+ */
+gulp.task('build-templates', ['pre-build-clean'], function () {
+  return gulp.src(paths.src.templates)
+    .pipe(gulp.dest(paths.app.root));
+});
+
+// e.g. dist-templates
+function prepareTemplates () {
+  return gulp.src(paths.src.templates)
+    .pipe(angularTemplateCache());
+}
+
+/**
+ * TASK: PREPARE PARTIALS
+ */
+gulp.task('build-partials', ['pre-build-clean'], function () {
+  return gulp.src(paths.src.partials)
+    .pipe(gulp.dest(paths.app.root));
+});
+
+gulp.task('dist-partials', ['pre-dist-clean'], function () {
+  return gulp.src(paths.src.partials)
+    .pipe(gulp.dest(paths.dist.components));
+});
+
+/**
+ * TASK: CLEAN
+ */
+gulp.task('pre-build-clean', function (cb) {
+  if (!isWatching) {
+    del(['app/*'], cb)
+  }
+  else cb();
+});
+
+gulp.task('pre-dist-clean', function (cb) {
+  del(['dist/*'], cb)
+});
+
